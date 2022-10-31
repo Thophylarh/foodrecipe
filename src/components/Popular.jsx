26a1d511a54a01d7f9c2a871a76771f4bc0385f5
Thophylarh <iwualathophylarh@gmail.com>
2022-10-31 12:55:23 +0100

    first commit

---
+++ src/components/Popular.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+function Popular() {
+
+useEffect(() => {
+getPopular();
+},[]);
+
+const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_API_KEY}`)
+    const data = await api.json();
+    console.log(data);
+}
+
+
+  return (
+    <div>Popular</div>
+  )
+}
+
+export default Popular
